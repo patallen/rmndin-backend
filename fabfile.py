723fe14e63fad269da5ea72fa64fa2d7bd950453
Patrick Allen <prallen90@gmail.com>
2017-01-16 16:51:58 -0500

    Add fab commands for db migrate and upgrade

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import env, run, cd
 from fabric.network import ssh
 
 ssh.util.log_to_file("paramiko.log", 10)
+env.use_ssh_config = True
 
 env.hosts = ['10.10.10.2']
 env.user = 'vagrant'
@@ -11,6 +12,13 @@ env.password = 'vagrant'
 def runserver():
     run("/var/rmndinenv/bin/python /var/rmndin/manage.py runserver")
 
+def db_migrate():
+    with cd('/var/rmndin'):
+        run("/var/rmndinenv/bin/python /var/rmndin/manage.py db migrate")
+
+def db_upgrade():
+    with cd('/var/rmndin'):
+        run("/var/rmndinenv/bin/python /var/rmndin/manage.py db upgrade")
 
 def celery_worker():
     with cd('/var/rmndin'):
