00af8857b26df5e077a0e18afa45703886341bca
Patrick Allen <prallen90@gmail.com>
2017-01-16 15:19:21 -0500

    Remove old test tasks

--- project/manage.py
+++ project/manage.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 manager = Manager(app)
 manager.add_command("runserver", Server())
 
-
-@manager.command
-def add():
-    from tasks import add, cancel_task
-    print "Adding!"
-    result = add.apply_async(countdown=10, args=(10, 10))
-    print result
-    cancel_task(result.id)
-
 if __name__ == '__main__':
     manager.run()
--- project/tasks.py
+++ project/tasks.py
@@ -4,11 +4,18 @@ celery = Celery('tasks', broker='pyamqp://localhost')
 
 
 @celery.task
-def add(x, y):
-    """Add 2 params together."""
-    return x + y
+def schedule_reminder(reminder):
+    """Add 2 kwargs together."""
+    func = get_reminder_function(reminder)
+    kwargs = get_reminder_kwargs(reminder)
+    eta = get_reminder_eta(reminder)
+
+    result = func.apply_async(eta, kwargs=kwargs)
+
+    return result
 
 
-def cancel_task(task_id):
+def cancel_reminder(reminder):
     """Terminate a queued task."""
+    task_id = reminder.task_id
     celery.control.revoke([task_id], terminate=True)
