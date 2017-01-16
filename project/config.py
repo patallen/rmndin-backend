ff959c31947d9ad6fcc88a61beda30096033a2b1
Patrick Allen <prallen90@gmail.com>
2017-01-16 15:53:19 -0500

    Set up postgres and flask_migrate

---
+++ project/config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = "posgresql://rmndinuser:rmndinpassword@localhost/rmndindb"
+SECRET_KEY = 'this is a realllyyy good secret key'
--- project/manage.py
+++ project/manage.py
@@ -1,10 +1,11 @@
 from flask_script import Manager, Server
-from flask import Flask
-# from api import app
+from flask_migrate import Migrate, MigrateCommand
+from api import app, db
 
-app = Flask(__name__)
 manager = Manager(app)
 manager.add_command("runserver", Server())
+manager.add_command("db", MigrateCommand)
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     manager.run()
