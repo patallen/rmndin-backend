472f9a31dba7a08eed66bdbb1cc9670bfa3321a8
Patrick Allen <prallen90@gmail.com>
2017-01-28 14:38:21 -0500

    Use private.yaml file for secret config
    - Add pyyaml to requirements
    - Add yaml_update_config function
    - Put private variables in private.yaml

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import yaml
+
+
+def yaml_update_config(app, path):
+    with open(path, 'r') as yfile:
+        ycontents = yaml.load(yfile)
+        app.config.update(ycontents)
+
 app = Flask(__name__)
-app.config.from_pyfile('../config.py')
+app.config.from_pyfile('/var/rmndin/config.py')
+yaml_update_config(app, '/var/rmndin/private.yaml')
 db = SQLAlchemy(app)
 
 from views.reminders import reminders as remindersbp
--- project/config.py
+++ project/config.py
@@ -1,3 +1,2 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://rmndinuser:rmndinpassword@localhost/rmndindb"
-SECRET_KEY = 'this is a realllyyy good secret key'
 DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
