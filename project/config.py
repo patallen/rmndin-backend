9d4f38efaa6fda3471bc4df47e15b5225deb8876
Patrick Allen <prallen90@gmail.com>
2017-01-29 20:56:05 -0500

    Update config with URL base

--- project/config.py
+++ project/config.py
@@ -1,5 +1,13 @@
+from datetime import timedelta
+
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if DEBUG:
-	JWT_EXPIRATION_DELTA = 24 * 60 * 60
+    JWT_EXPIRATION_DELTA = timedelta(seconds=24 * 60 * 60)
+
+CONTACT_VERIFY_MAX_AGE = 24 * 2 * 60 * 60
+
+URLS = {
+    "BASE_URL": 'http://rmndin.dev'
+}
