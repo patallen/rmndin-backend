f9331e32e7c2f189e820e57580f49cba46ebc48b
Patrick Allen <prallen90@gmail.com>
2017-02-02 22:51:20 -0500

    Return the response from send_email

--- rmndin/rmndin/services/email.py
+++ rmndin/rmndin/services/email.py
@@ -2,7 +2,7 @@ from flask import render_template
 from jinja2.exceptions import TemplateNotFound
 
 from rmndin import app
-import rmndin.lib.email
+from rmndin.lib import email
 
 
 def send_email(recipients, subject, from_address, text=None, html=None):
@@ -18,9 +18,9 @@ def send_email(recipients, subject, from_address, text=None, html=None):
     """
     apikey = app.config.get('MAILGUN_API_KEY')
     base_url = app.config.get('MAILGUN_BASE_URL')
-    rmndin.lib.email.send_email(recipients=recipients, subject=subject,
-                                from_address=from_address, apikey=apikey,
-                                base_url=base_url, text=text, html=html)
+    return email.send_email(recipients=recipients, subject=subject,
+                            from_address=from_address, apikey=apikey,
+                            base_url=base_url, text=text, html=html)
 
 
 def send_template_email(recipients, subject,
@@ -33,6 +33,8 @@ def send_template_email(recipients, subject,
     :param from_address: <str> Email you will appear as to recipient
     :param variables: <dict> containing key/val arguments for the template
     :param template: <str> designating the template path without extension.
+
+    :return: Response
     """
     try:
         html = render_template("%s.html" % template, **variables)
@@ -44,7 +46,7 @@ def send_template_email(recipients, subject,
     except TemplateNotFound:
         text = None
 
-    if not text or html:
+    if not (text or html):
         raise TemplateNotFound("No template found with base path '%s'." %
                                template)
     return send_email(recipients=recipients, subject=subject,
