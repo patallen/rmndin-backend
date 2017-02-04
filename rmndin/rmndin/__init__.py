6590787630de898153e02a2460912a0c355fb454
Patrick Allen <prallen90@gmail.com>
2017-02-04 17:01:18 -0500

    Prefix reminders blueprint properly

--- rmndin/rmndin/__init__.py
+++ rmndin/rmndin/__init__.py
@@ -14,7 +14,6 @@ app.config.from_pyfile('/var/rmndin/config.py')
 yaml_update_config(app, '/var/rmndin/private.yml')
 db = SQLAlchemy(app)
 
-
 from rmndin.auth import jwt
 jwt = JWT(app, jwt.authenticate, jwt.identity)
 
@@ -24,7 +23,7 @@ from rmndin.users.views import usersbp as users
 from rmndin.users.views import verifybp as verify
 from rmndin.auth.views import bp as auth
 
-app.register_blueprint(reminders, url_prefix='/api/reminders')
+app.register_blueprint(reminders, url_prefix='/api/users/<user_id>/reminders')
 app.register_blueprint(users, url_prefix='/api/users')
 app.register_blueprint(verify, url_prefix='/verify')
 app.register_blueprint(auth)
