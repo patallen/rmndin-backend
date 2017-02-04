2aefb41fa73af565239fd16257ea8fdcb33684e5
Patrick Allen <prallen90@gmail.com>
2017-02-04 13:38:17 -0500

    Update schedule_reminder task to use new Reminder model

--- rmndin/rmndin/tasks.py
+++ rmndin/rmndin/tasks.py
@@ -11,11 +11,11 @@ def schedule_reminder(reminder_id):
 def send_reminder(reminder_id):
     # get the reminder
     reminder = Reminder.query.get(reminder_id)
-    url = reminder.body
+    url = reminder.url
     user = reminder.user
+    created = reminder.created
     # send_website_email(user)
     # send email & make sure sent
     print "Sending email for %s" % url
     reminder.fulfilled = True
-    db.session.add(reminder)
-    db.session.commit()
+    reminder.save()
