fd5ca71ce2f0278967c509495058d975c85e4c85
Patrick Allen <prallen90@gmail.com>
2017-02-03 23:41:45 -0500

    Add reminder_contact many-to-many table

--- rmndin/rmndin/reminders/models.py
+++ rmndin/rmndin/reminders/models.py
@@ -2,6 +2,13 @@ from rmndin import db
 from rmndin.lib.db.mixins import BaseMixin
 
 
+reminder_contact = db.Table(
+    'reminder_contact_association',
+    db.Column('reminder_id', db.Integer, db.ForeignKey('reminder.id')),
+    db.Column('user_contact_id', db.Integer, db.ForeignKey('user_contact.id'))
+)
+
+
 class Reminder(BaseMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -9,3 +16,26 @@ class Reminder(BaseMixin, db.Model):
     body = db.Column(db.String)
     eta = db.Column(db.DateTime, nullable=False)
     fulfilled = db.Column(db.Boolean, nullable=False, default=False)
+
+    contacts = db.relationship("UserContact", secondary=reminder_contact,
+                               backref="reminders")
+
+
+    def add_contact(self, contact, commit=True):
+        self.contacts.append(contact)
+        db.session.add(self)
+        if commit:
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+                raise
+
+    def add_contacts(self, contacts, commit=True):
+        for contact in contacts:
+            self.contacts.append(contact)
+
+        self.session.add(self)
+
+        if commit:
+            self.session.commit()
