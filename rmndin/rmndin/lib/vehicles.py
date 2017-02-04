acaf13dc294132c88ba9dfa5b8d9c8f7bdf9355e
Patrick Allen <prallen90@gmail.com>
2017-02-03 19:10:35 -0500

    Use ABC to make ContactVehicle abstract

--- rmndin/rmndin/lib/vehicles.py
+++ rmndin/rmndin/lib/vehicles.py
@@ -1,4 +1,7 @@
 """Why the hell do I need a docstring here?."""
+
+from abc import ABCMeta, abstractmethod
+
 import praw
 from rmndin import app
 from rmndin.lib.verification import contact_verify_url
@@ -16,19 +19,27 @@ reddit = praw.Reddit(user_agent=app.config['REDDIT_USER_AGENT'],
 class ContactVehicle(object):
     """Base Vehicle."""
 
+    __metaclass__ = ABCMeta
+
     def __init__(self, user_contact, verified):
-        """Init."""
+        """Initialize the ContactVehicle."""
         self.contact = user_contact
         self.verified = verified
-        self.client = None
-        pass
 
+    @abstractmethod
     def send_reminder(self):
-        """Send the message to the proper user."""
+        """Send the reminder message for the specific service."""
         pass
 
+    @abstractmethod
     def send_verification(self):
+        """Send the verification link for the DeliveryType service."""
         pass
+
+    @property
+    def identifier(self):
+        """Shortcut for getting the contact's identifier."""
+        return self.contact.identifier
 
 
 class RedditContactVehicle(ContactVehicle):
@@ -38,23 +49,22 @@ class RedditContactVehicle(ContactVehicle):
         """
         Initialize the Reddit Vehicle.
 
-        :identifier - name of user on reddit.
+        :param contact: - name of user on reddit.
+        :param verified: <bool> If the user has been verified.
         """
         super(RedditContactVehicle, self).__init__(*args, **kwargs)
         self.client = reddit
 
     def send_reminder(self, message):
-        """
-        Sends
-        """
-        print "Sending reddit reminder to %s" % self.contact.identifier
+        """Send a reminder via reddit messaging."""
+        print "Sending reddit reminder to %s" % self.identifier
 
     def send_verification(self):
         """Send verification email to the UserContact's reddit inbox."""
         secret_key = app.config['CONTACT_VERIFY_SECRET']
         base_url = app.config['URLS']['BASE_URL']
         redditor = praw.models.Redditor(self.client,
-                                        name=self.contact.identifier)
+                                        name=self.identifier)
         verify_url = contact_verify_url(self.contact.id, base_url, secret_key)
         redditor.message("Verify your username!", verify_url)
 
@@ -64,18 +74,16 @@ class EmailContactVehicle(ContactVehicle):
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize the Email Vehicle.
+        Initialize the EmailContactVehicle.
 
-        :param identifier: <str> Email address for UserContact.
-        :param verified: <bool> If the user has been verified.
+        :param identifier: <UserContact>
+
         """
         super(EmailContactVehicle, self).__init__(*args, **kwargs)
 
     def send_reminder(self, message):
-        """
-        Sends
-        """
-        print "Sending reddit reminder to %s" % self.contact.identifier
+        """Send a reminder via email."""
+        print "Sending reddit reminder to %s" % self.identifier
 
     def send_verification(self):
         """Send verification email to the UserContact's email."""
@@ -86,7 +94,7 @@ class EmailContactVehicle(ContactVehicle):
             "username": self.contact.user.username,
             "verify_link": verify_url
         }
-        send_template_email(recipients=[self.contact.identifier],
+        send_template_email(recipients=[self.identifier],
                             subject="Verify your Rmnd.in Contact",
                             from_address="accounts@rmnd.in",
                             variables=variables,
